302bbb8141afd22069ffbb9f9f11dc936c31bec2
Najmiyatul Nabilah <najmiyatul2004@gmail.com>
2024-03-11 22:50:21 +0700

    tugas 2

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer footer-center p-4 bg-emerald-600 text-base-content">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Najmiyatul Nabilah</p>
+    </aside>
+  </footer>
+ 
+  {{-- <footer class="py-3 text-center text-white">
+    <p class="m-0">Copyright © 2024 - All right reserved by Najmiyatul Nabilah</p>
+  </footer> --}}
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,9 @@
+<div class="navbar bg-emerald-600 text-neutral-content flex justify-center">
+    <div class="navbar-start">
+    </div>
+    <div class="navbar-center">
+      <a href="/" class="btn btn-ghost text-xl text-black hover:bg-transparent">To Do List</a>
+    </div>
+    <div class="navbar-end">
+    </div>
+  </div>
